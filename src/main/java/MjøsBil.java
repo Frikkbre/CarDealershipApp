2589778e2118114be8e93c415c22aebb366644d7
Frikk Brændsrød <fribra05@yahoo.com>
2024-11-04 11:33:31 +0100

    Added UI for Mjøs Bil

--- src/main/java/MjøsBil.java
+++ src/main/java/MjøsBil.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
 public class MjøsBil {
+
+
+    public static void main(String[] args) {
+
+        HashMap<String, Car> mjøsBilRegistry = new HashMap<>();
+
+        CarInventory mjøsBilRegistry = new HashMap<Car>();
+    }
 }
